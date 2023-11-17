57fcb50f5fe4754ad1dd898423ec4c4d95e2fe37
Nathan Altice <naltice@ucsc.edu>
2023-11-17 10:29:06 -0800

    initial commit

---
+++ src/Overworld.js
@@ -0,0 +1,50 @@
+// Exercise 04: Slime World
+// Name: 
+// Date: 
+
+class Overworld extends Phaser.Scene {
+    constructor() {
+        super('overworldScene')
+    }
+
+    preload() {
+        this.load.path = './assets/'
+        this.load.spritesheet('slime', 'slime.png', {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+    }
+
+    create() {
+        // velocity constant
+        this.VEL = 100
+
+        // add slime
+        this.slime = this.physics.add.sprite(32, 32, 'slime', 0)
+        this.slime.body.setCollideWorldBounds(true)
+
+        // slime animation
+
+        // input
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // slime movement
+        this.direction = new Phaser.Math.Vector2(0)
+        if(this.cursors.left.isDown) {
+            this.direction.x = -1
+        } else if(this.cursors.right.isDown) {
+            this.direction.x = 1
+        }
+
+        if(this.cursors.up.isDown) {
+            this.direction.y = -1
+        } else if(this.cursors.down.isDown) {
+            this.direction.y = 1
+        }
+
+        this.direction.normalize()
+        this.slime.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y)
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,18 @@
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
